8bc2eda8d4f18a2369dd6b7fea7624fe658993f8
kapilTantuway <kapil.kumar.svp@gmail.com>
2024-03-19 14:52:14 +0530

    Redirects

--- challenges/views.py
+++ challenges/views.py
@@ -1,17 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 # Create your views here.
+
+monthly_releases = {
+    "january": ["Attack on Titan Final Season Part 2", "Komi Can't Communicate"],
+    "february": ["Demon Slayer: Kimetsu no Yaiba Entertainment District Arc", "The Case Study of Vanitas Part 2"],
+    "march": ["JoJo's Bizarre Adventure Part 6: Stone Ocean", "The Rising of the Shield Hero Season 2"],
+    "april": ["My Hero Academia Season 6", "Tokyo Revengers Part 2"],
+    "may": ["One Piece: Romance Dawn", "Edens Zero"],
+    "june": ["Jujutsu Kaisen 0 (Movie)", "To Your Eternity Season 2"],
+    "july": ["The World's Finest Assassin Gets Reincarnated in Another World as an Aristocrat", "The Duke of Death and His Maid"],
+    "august": ["Re:Zero - Starting Life in Another World Season 3", "That Time I Got Reincarnated as a Slime Season 3"],
+    "september": ["Bleach: Thousand-Year Blood War Arc", "Banished from the Heroes' Party"],
+    "october": ["Black Clover: Quartet Knights", "Platinum End"],
+    "november": ["Dragon Ball Super: Super Hero (Movie)", "Mushoku Tensei: Jobless Reincarnation Season 2"],
+    "december": ["Sword Art Online Progressive: Aria of a Starless Night", "The Faraway Paladin"]
+}
 
 
 def month_challenges_by_numbers(request, month):
-    return HttpResponse(month)
+    months = list(monthly_releases.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Not Supported")
+
+    forward_month = months[month-1]
+    return HttpResponseRedirect("/challenges/"+forward_month)
+
 
 def month_challenges(request, month):
-    content = None
-    if month == 'january':
-        content = 'This is a challenge for january'
-    elif month == 'february':
-        content = 'You made it to february.'
-    if content:
+    try:
+        content = monthly_releases[month]
         return HttpResponse(content)
-    return HttpResponseNotFound('This is not supported')
+    except KeyError:
+        return HttpResponseNotFound("Mentioned release is not available")
