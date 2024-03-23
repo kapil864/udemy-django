d55f7f27c27b7a84f7cbe4df83f1ebcebd094976
kapilTantuway <kapil.kumar.svp@gmail.com>
2024-03-23 12:20:04 +0530

    Updated bolg site with fetching data from database

--- BlogSite/blog/admin.py
+++ BlogSite/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Actor, Director, Writer, Genre, Movie
 
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('title',)}
+    list_display = ('title','director')
+    list_filter = ('director','released')
+
+admin.site.register(Actor)
+admin.site.register(Director)
+admin.site.register(Writer)
+admin.site.register(Genre)
+admin.site.register(Movie, MovieAdmin)
--- BlogSite/blog/views.py
+++ BlogSite/blog/views.py
@@ -1,29 +1,30 @@
 from django.shortcuts import render
 from .models import movies_data
 from django.http import Http404
-
+from .models import Movie
 
 # Create your views here.
 
 
 def starting_page(request):
     try:
-        latest_movies = movies_data[:-5:-1]
+        latest_movies = Movie.objects.all().order_by('-released')[0:2]
         return render(request, 'blog/welcome.html', {'latest_movies': latest_movies})
     except:
-        return Http404()
+        raise Http404()
 
 
 def releases(request):
     try:
+        movies_data = Movie.objects.all()
         return render(request, 'blog/list_movies.html', {'movies': movies_data})
     except:
-        return Http404()
+        raise Http404()
 
 
 def release_detail(request, slug):
     try:
-        movie = next(movie for movie in movies_data if movie['Title'] == slug)
+        movie = Movie.objects.get(slug=slug)
         return render(request, 'blog/movie.html', {'movie': movie})
     except:
-        return Http404()
+        raise Http404()
