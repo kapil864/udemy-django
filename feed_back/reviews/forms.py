d1613e7d47d6e26c884d8c916359451d9c88960e
kapilTantuway <kapil.kumar.svp@gmail.com>
2024-03-23 15:44:04 +0530

    Customize rendered HTML

--- feed_back/reviews/forms.py
+++ feed_back/reviews/forms.py
@@ -4,4 +4,4 @@ class ReviewForm(forms.Form):
     user_name = forms.CharField(label='Your name', max_length=100, error_messages= {
         'required' : 'Name must not be empty',
         'max_length' : 'Enter a shorter name '
-    })
+    } )
