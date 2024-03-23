21f684e6b556bd1eabba7f053e36446722c1e131
kapilTantuway <kapil.kumar.svp@gmail.com>
2024-03-23 14:04:49 +0530

    Extracting data from POST request

--- feed_back/reviews/urls.py
+++ feed_back/reviews/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', view=views.review)
+    path('', view=views.review),
+    path('thank-you', view=views.thankyou)
 ]
--- feed_back/reviews/views.py
+++ feed_back/reviews/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def review(request):
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+        return HttpResponseRedirect('/thank-you')
     return render(request, 'reviews/review.html')
+
+
+def thankyou(request):
+    return render(request, 'reviews/thank_you.html')
