eda93071b75cf61e3361182728c74b3a30c7b50f
kapilTantuway <kapil.kumar.svp@gmail.com>
2024-03-24 18:44:16 +0530

    Updated view to class based views

--- BlogSite/blog/urls.py
+++ BlogSite/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.starting_page,name='release-index'),
-    path('releases/',views.releases, name='all-releases'),
-    path('releases/<str:slug>',views.release_detail, name='a-release')
+    path('', views.StaringView.as_view(), name='release-index'),
+    path('releases/', views.ReleasesView.as_view(), name='all-releases'),
+    path('releases/<str:slug>', views.ReleaseDetailView.as_view(), name='a-release')
 ]
--- BlogSite/blog/views.py
+++ BlogSite/blog/views.py
@@ -2,29 +2,28 @@ from django.shortcuts import render
 from .models import movies_data
 from django.http import Http404
 from .models import Movie
+from django.views.generic import View
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
 
-def starting_page(request):
-    try:
-        latest_movies = Movie.objects.all().order_by('-released')[0:2]
-        return render(request, 'blog/welcome.html', {'latest_movies': latest_movies})
-    except:
-        raise Http404()
+class StaringView(View):
+
+    def get(self, request):
+        try:
+            latest_movies = Movie.objects.all().order_by('-released')[0:2]
+            return render(request, 'blog/welcome.html', {'latest_movies': latest_movies})
+        except:
+            raise Http404()
+
+class ReleasesView(ListView):
+    model = Movie
+    template_name = 'blog/list_movies.html'
+    context_object_name= 'movies'
 
 
-def releases(request):
-    try:
-        movies_data = Movie.objects.all()
-        return render(request, 'blog/list_movies.html', {'movies': movies_data})
-    except:
-        raise Http404()
-
-
-def release_detail(request, slug):
-    try:
-        movie = Movie.objects.get(slug=slug)
-        return render(request, 'blog/movie.html', {'movie': movie})
-    except:
-        raise Http404()
+class ReleaseDetailView(DetailView):
+    model = Movie
+    template_name = 'blog/movie.html'
+    context_object_name = 'movie'
