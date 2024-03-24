5586db11977ab56b36fbffe710c09c5f754aa6da
kapilTantuway <kapil.kumar.svp@gmail.com>
2024-03-24 14:29:18 +0530

    Using session data

--- feed_back/reviews/urls.py
+++ feed_back/reviews/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('', view=views.ReviewView.as_view()),
     path('thank-you', view=views.ThankYouView.as_view()),
     path('reviews', view=views.ReviewListView.as_view()),
+    path('reviews/favorite', view=views.AddFavoriteView.as_view()),
     path('reviews/<int:pk>', view=views.SingleReviewView.as_view())
 ]
