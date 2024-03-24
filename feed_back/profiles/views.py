ba50bc803802283309795e1a9585a6a3a624927f
kapilTantuway <kapil.kumar.svp@gmail.com>
2024-03-24 12:28:24 +0530

    Upload using createView

--- feed_back/profiles/views.py
+++ feed_back/profiles/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic import CreateView
 
 
 from .forms import ProfileForm
 from .models import Profile
 # Create your views here.
 
-class CreateProfileView(View):
+class CreateProfileView(CreateView):
 
-    def get(self, request):
-        form = ProfileForm
-        return render(request, 'profiles/create_profile.html', {'form' : form})
+    template_name = 'profiles/create_profile.html'
+    model=Profile
+    fields = '__all__'
+    success_url = 'profiles/create_profile.html'
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
-        if submitted_form.is_valid():
-            profile = Profile(image=request.FILES['user_image'])
-            profile.save()
-            return HttpResponseRedirect('/profiles')
-        
-        return render(request, 'profiles/create_profile.html', {'form': submitted_form})
+    # can add form class if we want to customize form
+    # form_class = ProfileForm
