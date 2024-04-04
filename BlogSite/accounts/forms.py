6424d479d7b1407c4de768e2627a5af8452a44f9
kapilTantuway <kapil.kumar.svp@gmail.com>
2024-04-04 18:28:37 +0530

    Added login logout and signup functionality

--- BlogSite/accounts/forms.py
+++ BlogSite/accounts/forms.py
@@ -2,4 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
-    pass
+    
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
